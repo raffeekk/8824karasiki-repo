f2faddfdf28b2ce5226cb9487e9ddebdb04f9905
raffeekk <idmitryfrl@gmail.com>
2024-12-06 14:48:08 +0300

    initialize application

---
+++ backend/src/main/java/so/karasiki/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package so.karasiki.backend.repository;
+
+import so.karasiki.backend.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
